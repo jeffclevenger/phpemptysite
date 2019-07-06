f8d75e832fffc2696f36a88318e01e3724ddf411
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2019-07-05 22:43:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $today = date("F j, Y, g:i a");
 $data = json_encode(array("headers" =&amp;amp;gt; getallheaders(), "server" =&amp;amp;gt; $_SERVER, "request" =&amp;amp;gt; $_REQUEST));
-file_put_contents("summary.txt", $today . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\n", FILE_APPEND);
-file_put_contents("full-data.txt", $data . "\n", FILE_APPEND);
+//file_put_contents("summary.txt", $today . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\n", FILE_APPEND);
+//file_put_contents("full-data.txt", $data . "\n", FILE_APPEND);
 //$message = $today . " - " . $_SERVER['REMOTE_ADDR'];
 //mail('5105551212@tmomail.net', 'subject', $message);
 ?>
