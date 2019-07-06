728388cece0a4bee99301bcd88566fc86b764b20
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2019-07-05 23:13:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $today = date("F j, Y, g:i a");
-$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]" . " - $_SERVER['HTTP_USER_AGENT'];
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]"  . " - $_SERVER[REMOTE_HOST]" .  " - $_SERVER[REMOTE_USER]" . " - $_SERVER[HTTP_USER_AGENT]" . " - $_SERVER[SERVER_NAME]";
 file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
 echo "$today"
 
