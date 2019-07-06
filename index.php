d8e33aad8c6b76431f8cf4f3291e08c6c07113d6
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2019-07-05 22:47:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $today = date("F j, Y, g:i a");
-$data = json_encode(array("headers" =&amp;amp;gt; getallheaders(), "server" =&amp;amp;gt; $_SERVER, "request" =&amp;amp;gt; $_REQUEST));
+$data = json_encode(array("headers" =&amp;amp;gt; getallheaders());
+                          //, "server" =&amp;amp;gt; $_SERVER, "request" =&amp;amp;gt; $_REQUEST));
 echo "$today"
   echo "$data"
 //file_put_contents("summary.txt", $today . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . $_SERVER['HTTP_USER_AGENT'] . "\n", FILE_APPEND);
