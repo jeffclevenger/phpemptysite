0e8baadb630385ca29c5c03910291244914fe43b
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2019-07-05 22:56:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 //include ('log-ip.php')
 $today = date("F j, Y, g:i a");
+
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
+file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
+
+echo $line
 //$data = json_encode(array("headers" =&amp;amp;gt; getallheaders());
                           //, "server" =&amp;amp;gt; $_SERVER, "request" =&amp;amp;gt; $_REQUEST));
 echo "$today"
