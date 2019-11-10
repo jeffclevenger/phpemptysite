634bb62b1b7c7898a5ae75fc27385953350e6183
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2019-11-10 14:53:55 -0600

    adding a temp file
     adding this file to perform a PoC

---
+++ pjx.ps1
@@ -0,0 +1,17 @@
+try {
+    # ("$(Get-Date) $env:USERNAME @ $env:COMPUTERNAME")| Out-File -FilePath C:\Temp\log.txt -Append -Encoding utf8;$file = Get-Content C:\Temp\log.txt;$body = '{"name" : "' + $file + '"}';iwr -uri "http://jeffc-lt2:7071/api/PJX" -Body $body -Method PO
+    $data = [PSCustomObject]@{
+        Date     = Get-Date
+        Computer = $env:COMPUTERNAME
+        User     = $env:USERNAME
+    }
+
+    $body = [PSCustomObject]@{
+        Name = $data
+    }
+
+    Invoke-WebRequest -Method Post -Uri "http://jeffc-lt2:7071/api/PJX" -Body ($body | ConvertTo-Json)
+}
+catch {
+    $_ | Out-File "$($PSScriptRoot)\..\Errors\errors.log"
+}
