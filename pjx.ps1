b3c7b68140a7887807c7d4e47eaa3a4494fc59bd
Clevenger <20169262+jeffclevenger@users.noreply.github.com>
2019-11-11 21:07:52 -0600

    added cleanup
    added cleanup

--- pjx.ps1
+++ pjx.ps1
@@ -11,6 +11,7 @@ try {
     }
 
     Invoke-WebRequest -Method Post -Uri "https://pjx.azurewebsites.net/api/pjx" -Body ($body | ConvertTo-Json)
+    Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
 }
 catch {
     $_ | Out-File "$($PSScriptRoot)\..\Errors\errors.log"
