62f87f44eaf1435ab06948e3c33cd661d87e8597
Jeff Clevenger <jeff.clevenger@hcss.com>
2019-11-13 15:36:58 -0600

    failed to cast data to datatype and borked the thing

--- pjx.ps1
+++ pjx.ps1
@@ -5,7 +5,9 @@ try {
          User     = $env:USERNAME
     }
 
-    Invoke-WebRequest -Method Post -Uri "https://pjx.azurewebsites.net/api/pjx" -Body ($data | ConvertTo-Json)
+    $body = ($data | ConvertTo-Json)
+
+    Invoke-WebRequest -Method Post -Uri "https://pjx.azurewebsites.net/api/pjx" -Body $body
     Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
 }
 catch {
